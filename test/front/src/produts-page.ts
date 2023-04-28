0bb0f701fdd9108a3fa063224fa448fd8d064b2d
yersultan <erserstur@gmail.com>
2023-04-29 00:37:55 +0600

    second

---
+++ test/front/src/produts-page.ts
@@ -0,0 +1,16 @@
+export interface Product{
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products = [
+    {
+        id: 1,
+        name: 'Ноутбук',
+        price: 545000,
+        description: '',
+    },
+    
+]
